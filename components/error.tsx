7285ee0cd37edd8151a71f75dfe34fea3d7219e9
Òscar Casajuana <elboletaire@gmail.com>
2021-03-30 13:25:41 +0200

    Minor i18n change forgot in previous commit

--- components/error.tsx
+++ components/error.tsx
@@ -1,7 +1,7 @@
 import i18n from '../i18n'
 
 const ErrorPage = (props: { message?: string }) => (
-  <div>{(props && props.message) || i18n.t('errors.generalErrorMessage')}</div>
+  <div>{(props && props.message) || i18n.t('errors.general_error')}</div>
 )
 
 export default ErrorPage
