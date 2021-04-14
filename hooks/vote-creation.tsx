b5e3658403aa56c1a1e3083e0b6756156557112c
emmdim <emmdim@users.noreply.github.com>
2021-04-14 02:45:33 +0200

    WIP

--- hooks/vote-creation.tsx
+++ hooks/vote-creation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { usePool } from '@vocdoni/react-hooks'
 import {
   CensusOffChainApi,
@@ -7,17 +8,18 @@ import {
   ProcessMetadataTemplate,
   ProcessMode,
   VotingApi,
+  INewProcessParams
 } from 'dvote-js'
 import { createContext, useContext, useState } from 'react'
 import { VoteCreationSteps } from '../components/steps-new-vote'
-import { forceUpdate } from "../hooks/force-update"
+// import { forceUpdate } from "../hooks/force-update"
 import i18n from '../i18n'
 import { uploadFileToIpfs } from '../lib/file'
 import { StepperFunc, StepperFuncResult } from '../lib/types'
 import { extractDigestedPubKeyFromString, importedRowToString } from '../lib/util'
 import { useStepper } from './use-stepper'
 import { useWallet } from './use-wallet'
-
+import { useNewVotingProcess } from './use-new-voting-process'
 export interface VoteCreationContext {
   metadata: ProcessMetadata,
   parameters: ProcessContractParameters,
@@ -41,19 +43,12 @@ export const useVoteCreation = () => {
 }
 
 export const UseVoteCreationProvider = ({ children }) => {
-  const [metadata, setMetadata] = useState<ProcessMetadata>(() => JSON.parse(JSON.stringify(ProcessMetadataTemplate)))
-  const [headerUrl, setHeaderUrl] = useState<string>("")
+
+  const { id, parameters, metadata, address, setID, setParameters, setMetadata, setAddress  } = useNewVotingProcess()
   const [spreadsheetData, setSpreadsheetData] = useState<string[][]>()
   const [headerFile, setHeaderFile] = useState<File>()
   const { wallet } = useWallet()
   const { pool, poolPromise } = usePool()
-  const [parameters, setParameters] = useState<ProcessContractParameters>(() => {
-    const defValue = new ProcessContractParameters()
-    defValue.mode = new ProcessMode(ProcessMode.make({ autoStart: true }))
-    defValue.envelopeType = new ProcessEnvelopeType(ProcessEnvelopeType.make({ encryptedVotes: false }))
-    defValue.paramsSignature = '0x000000000000000000000'
-    return defValue
-  })
 
   // meta
   const setTitle = (title: string) => {
@@ -67,9 +62,9 @@ export const UseVoteCreationProvider = ({ children }) => {
   // params (we need to use forceUpdate, since the reference cannot be changed unless creating a new instance and copying everything)
   const setMode = (mode: ProcessMode) => {
     if (parameters.mode == mode) return
-    parameters.mode = mode
-    forceUpdate()
+    setParameters({...parameters,  mode})
   }
+
 
   const censusCreation: StepperFunc = async () => {
     const name = metadata.title.default + '_' + Math.floor(Date.now() / 1000)
@@ -83,11 +78,12 @@ export const UseVoteCreationProvider = ({ children }) => {
     const { censusId } = await CensusOffChainApi.addCensus(name, [wallet._signingKey().publicKey], wallet, pool)
     const { censusRoot, invalidClaims } = await CensusOffChainApi.addClaimBulk(censusId, claims, true, wallet, pool)
     if (invalidClaims.length) {
-      return {error: i18n.t('error.invalid_claims_found', {total: invalidClaims.length})}
+      return { error: i18n.t('error.invalid_claims_found', { total: invalidClaims.length }) }
     }
     const censusUri = await CensusOffChainApi.publishCensus(censusId, wallet, pool)
-    parameters.censusRoot = censusRoot
-    parameters.censusUri = censusUri
+    setParameters({...parameters, censusRoot, censusUri })
+
+    // TODO
     // parameters.startBlock =
     // parameters.blockCount =
     // metadata.meta.formUri =
@@ -96,28 +92,32 @@ export const UseVoteCreationProvider = ({ children }) => {
   const processCreation: StepperFunc = (): Promise<StepperFuncResult> => {
     return poolPromise
       .then(async (pool) => {
-        metadata.media.header = headerUrl
+        let  headerUrl = ''
         if (headerFile) {
-          metadata.media.header = await uploadFileToIpfs(headerFile, pool, wallet)
+          headerUrl = await uploadFileToIpfs(headerFile, pool, wallet)
         }
         // ProcessContractParameters !== INewProcessParams
-        parameters.metadata = metadata
+        // parameters.metadata = metadata
         // Set proper maxValue and maxCount
         let maxValue = 0, maxCount = 0
         metadata.questions.forEach((question) => {
-            if (maxValue < question.choices.length) {
-                maxValue = question.choices.length
-            }
-            maxCount++
+          if (maxValue < question.choices.length) {
+            maxValue = question.choices.length
+          }
+          maxCount++
         })
-        parameters.maxValue = maxValue
-        parameters.maxCount = maxCount
 
+        setParameters({...parameters, maxValue, maxCount})
+        setMetadata({...metadata,  media: {header : headerUrl}})
         return pool
       })
       .then(async (pool) => {
         // ProcessContractParameters !== INewProcessParams
-        await VotingApi.newProcess(parameters.toContractParams(), wallet, pool)
+        const finalParams: INewProcessParams = {
+          ...parameters, metadata
+        }
+        const processUri = await VotingApi.newProcess(finalParams, wallet, pool)
+        console.debug("processUri:", processUri)
 
         return {
           waitNext: false,
