ad5d0aa5e9c597afcb70f8bd9456bce39607e59c
lales12 <alexflores120@gmail.com>
2021-04-19 17:05:49 +0200

    [VOC-57] Add loader to make transition smooth

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
+import { sizes } from '../theme/sizes'
+import { useWallet } from '../hooks/use-wallet'
+
 import { Header } from './header'
 import { Footer } from './footer'
 import { MessageAlert } from './msg-alert'
 import { LoadingAlert } from './loading-alert'
-import { sizes } from '../theme/sizes'
+import { Loader } from './loader'
+
 
 const LayoutContainer = styled.div`
   padding: 0 ${({ theme }) => theme.margins.mobile.horizontal};
@@ -19,12 +23,18 @@ const LayoutContainer = styled.div`
   }
 `
 
-export const Layout = ({ children }) => (
-  <>
-    <MessageAlert />
-    <LoadingAlert />
-    <Header />
-    <LayoutContainer>{children}</LayoutContainer>
-    <Footer />
-  </>
-)
+export const Layout = ({ children }) => {
+  const { loadingWallet } = useWallet();
+  
+  return (
+    <>
+      <MessageAlert />
+      <LoadingAlert />
+      <Header />
+      
+      <Loader visible={loadingWallet} />
+      <LayoutContainer>{children}</LayoutContainer>
+      <Footer />
+    </>
+  )
+}
