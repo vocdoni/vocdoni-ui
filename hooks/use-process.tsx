775af6c5b325b18a9a9f9cd25fadc6edc9d08e17
lales12 <alexflores120@gmail.com>
2021-04-19 22:38:45 +0200

    [VOC-57] Move retrieve process to hook

--- hooks/use-process.tsx
+++ hooks/use-process.tsx
@@ -1,51 +1,22 @@
+import { useEffect, useState } from 'react'
 import { usePool } from '@vocdoni/react-hooks'
-import { GatewayPool, ProcessStatus } from 'dvote-js'
+import { GatewayPool } from 'dvote-js'
 
 import { ProcessInfo } from '../lib/types'
 import { getProcesses } from '../lib/api'
 
-export interface IFilteredProcess {
-  activeVotes: ProcessInfo[],
-  upcomingVotes: ProcessInfo[],
-  voteResults: ProcessInfo[]
-}
 
-interface ICachedProcesses {
-  [key: string]: IFilteredProcess
-}
+export const useProcessesFromAccount = (accountAddress: string) => {
+  const [processes, setProcesses] = useState<ProcessInfo[]>([])
+  const { poolPromise } = usePool();
 
-const cachedProcesses: ICachedProcesses = {}
+  useEffect(() => {
+    poolPromise.then(async (pool: GatewayPool) => {
+      const accountProcesses = await getProcesses(accountAddress, pool)
 
-export const useProcess = () => {
-  const { poolPromise } = usePool()
+      setProcesses(accountProcesses);
+    }) 
+  }, [accountAddress])
 
-  const getAccountProcesses = async (accountAddress: string) => {
-    if (cachedProcesses[accountAddress]) return process[accountAddress]
-
-    const pool: GatewayPool = await poolPromise
-    const accountProcess = await getProcesses(accountAddress, pool)
-    
-    const activeVotes = accountProcess.filter(
-      (process) =>
-        process.parameters?.status.value === ProcessStatus.READY
-    )
-    const upcomingVotes = accountProcess.filter(
-      (process) =>
-        process.parameters?.status.value === ProcessStatus.ENDED
-    )
-    const voteResults = accountProcess.filter(
-      (process) =>
-        process.parameters?.status.value === ProcessStatus.RESULTS
-    )
-
-    cachedProcesses[accountAddress] = {
-      activeVotes,
-      upcomingVotes,
-      voteResults
-    }
-
-    return cachedProcesses[accountAddress]
-  }
-
-  return { getAccountProcesses }
+  return { processes }
 }
