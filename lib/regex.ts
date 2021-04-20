2ce83515707c0593407ec09bfc08e3b1a94b7cc1
Jør∂¡ <itsbrickpop@gmail.com>
2021-04-20 13:56:24 +0100

    WIP Creation hooks

--- lib/regex.ts
+++ lib/regex.ts
@@ -1,5 +1,6 @@
 const HEX_REGEX = /^(0x)?[0-9a-fA-F]+$/
 const EMAIL_REGEX = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i
+const URI_REGEX = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/
 
 export function isValidEmail(text: string): boolean {
   return EMAIL_REGEX.test(text)
@@ -7,4 +8,8 @@ export function isValidEmail(text: string): boolean {
 
 export function isValidHexString(text: string): boolean {
   return HEX_REGEX.test(text)
+}
+
+export function isUri(text: string): boolean {
+  return URI_REGEX.test(text)
 }
