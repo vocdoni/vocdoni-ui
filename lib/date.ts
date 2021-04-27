6b1468c14196607c4468d762b180fbff63071447
Jør∂¡ <itsbrickpop@gmail.com>
2021-04-27 11:21:41 +0100

    Adding localized date difference texts

--- lib/date.ts
+++ lib/date.ts
@@ -1,5 +1,5 @@
 import { GatewayPool, VotingApi } from "dvote-js"
-
+import i18n from "../i18n"
 import { ProcessInfo } from "./types"
 
 export enum DateDiffType {
@@ -7,7 +7,7 @@ export enum DateDiffType {
   End = 'end-date'
 }
 
-export const getRemainingDays = async (
+export const getDaysUntilEnd = async (
   process: ProcessInfo,
   pool: GatewayPool
 ): Promise<string> => {
@@ -15,58 +15,125 @@ export const getRemainingDays = async (
     process.parameters.startBlock + process.parameters.blockCount,
     pool
   )
-  const remainTime = strDateDiff(DateDiffType.End, endDate)
-
-  return remainTime
+  return localizedStrDateDiff(DateDiffType.End, endDate)
 }
 
-export function strDateDiff(type: DateDiffType, target: Date): string {
+export function localizedStrDateDiff(type: DateDiffType, target: Date): string {
   if (!target) return ''
   let diff = (target.getTime() - Date.now()) / 1000
-  let num: number, prefix: string
 
-  if (diff > 3) {
-    prefix = type == DateDiffType.Start ? 'starting' :
-      type == DateDiffType.End ? 'ending' : ''
+  if (diff > 3) return strDiffFuture(type, diff)
+  else if (diff < -3) return strDiffPast(type, -diff)
+  else if (type == DateDiffType.Start) return i18n.t("dates.starting_right_now")
+  return i18n.t("dates.ending_right_now")
+}
 
-    // future
-    if (diff > 60 * 60 * 24) {
-      num = Math.floor(diff / 60 / 60 / 24)
-      return num > 1 ? `${prefix} in ${num} days` : prefix + ' in 1 day'
-    } else if (diff > 60 * 60) {
-      num = Math.floor(diff / 60 / 60)
-      return num > 1 ? `${prefix} in ${num} hours` : prefix + ' in 1 hour'
-    } else if (diff > 60) {
-      num = Math.floor(diff / 60)
-      return num > 1 ? `${prefix} in ${num} minutes` : prefix + ' in 1 minute'
-    } else {
-      num = Math.floor(diff)
-      return `${prefix} in ${num} seconds`
+// Helpers
+
+function strDiffFuture(type: DateDiffType, secondDiff: number): string {
+  let num: number
+
+  if (secondDiff > 60 * 60 * 24) {
+    // days
+    num = Math.floor(secondDiff / 60 / 60 / 24)
+
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_n_days", { interpolation: { num } })
+      return i18n.t("dates.ending_in_n_days", { interpolation: { num } })
     }
-  } else if (diff < -3) {
-    prefix = type == DateDiffType.Start ? 'started' :
-      type == DateDiffType.End ? 'ended' : ''
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_tomorrow")
+      return i18n.t("dates.ending_tomorrow")
+    }
+  } else if (secondDiff > 60 * 60) {
+    // hours
+    num = Math.floor(secondDiff / 60 / 60)
 
-    diff = -diff
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_n_hours", { interpolation: { num } })
+      return i18n.t("dates.ending_in_n_hours", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_one_hour")
+      return i18n.t("dates.ending_in_one_hour")
+    }
+  } else if (secondDiff > 60) {
+    // minutes
+    num = Math.floor(secondDiff / 60)
 
-    // past
-    if (diff > 60 * 60 * 24) {
-      num = Math.floor(diff / 60 / 60 / 24)
-      return num > 1 ? `${prefix} ${num} days ago` : prefix + ' 1 day ago'
-    } else if (diff > 60 * 60) {
-      num = Math.floor(diff / 60 / 60)
-      return num > 1 ? `${prefix} ${num} hours ago` : prefix + ' 1 hour ago'
-    } else if (diff > 60) {
-      num = Math.floor(diff / 60)
-      return num > 1 ? `${prefix} ${num} minutes ago` : prefix + ' 1 minute ago'
-    } else {
-      num = Math.floor(diff)
-      return `${prefix} ${num} seconds ago`
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_n_minutes", { interpolation: { num } })
+      return i18n.t("dates.ending_in_n_minutes", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_one_minute")
+      return i18n.t("dates.ending_in_one_minute")
     }
   } else {
-    prefix = type == DateDiffType.Start ? 'starting' :
-      type == DateDiffType.End ? 'ending' : ''
+    // seconds
+    num = Math.floor(secondDiff)
 
-    return 'right now'
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_in_n_seconds", { interpolation: { num } })
+      return i18n.t("dates.ending_in_n_seconds", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.starting_right_now")
+      return i18n.t("dates.ending_right_now")
+    }
+  }
+}
+
+function strDiffPast(type: DateDiffType, secondDiff: number): string {
+  let num: number
+
+  if (secondDiff > 60 * 60 * 24) {
+    // days
+    num = Math.floor(secondDiff / 60 / 60 / 24)
+
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_n_days_ago", { interpolation: { num } })
+      return i18n.t("dates.ended_n_days_ago", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_yesterday")
+      return i18n.t("dates.ended_yesterday")
+    }
+  } else if (secondDiff > 60 * 60) {
+    // hours
+    num = Math.floor(secondDiff / 60 / 60)
+
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_n_hours_ago", { interpolation: { num } })
+      return i18n.t("dates.ended_n_hours_ago", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_an_hour_ago")
+      return i18n.t("dates.ended_an_hour_ago")
+    }
+  } else if (secondDiff > 60) {
+    // minutes
+    num = Math.floor(secondDiff / 60)
+
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_n_minutes_ago", { interpolation: { num } })
+      return i18n.t("dates.ended_n_minutes_ago", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_a_minute_ago")
+      return i18n.t("dates.ended_a_minute_ago")
+    }
+  } else {
+    // seconds
+    num = Math.floor(secondDiff)
+
+    if (num > 1) {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_n_seconds_ago", { interpolation: { num } })
+      return i18n.t("dates.ended_n_seconds_ago", { interpolation: { num } })
+    }
+    else {
+      if (type == DateDiffType.Start) return i18n.t("dates.started_right_now")
+      return i18n.t("dates.ended_right_now")
+    }
   }
 }
