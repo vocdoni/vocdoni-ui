a7d27a9c04ce1b369047d2a7cf95e91d032a382c
Jør∂¡ <itsbrickpop@gmail.com>
2021-05-12 12:31:36 +0200

    Image component supports other image types

--- components/common/image.tsx
+++ components/common/image.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FileApi } from 'dvote-js'
 import { usePool } from '@vocdoni/react-hooks'
 
@@ -26,13 +26,52 @@ export const Image = (props: IImageProps) => {
   useEffect(() => {
     if (ipfsRegex.test(props.src)) {
       poolPromise.then(async (pool) => {
-        const file = await FileApi.fetchBytes(props.src, pool.activeGateway)
-        const base64 = `data:image/png;base64,${file.toString('base64')}`
+        const fileBytes = await FileApi.fetchBytes(props.src, pool.activeGateway)
+        const mimeType = getMimeType(Uint8Array.from(fileBytes))
 
-        setImageSrc(base64)
+        const dataUrl = `data:${mimeType};base64,${fileBytes.toString('base64')}`
+        setImageSrc(dataUrl)
       })
+    }
+    else {
+      setImageSrc(props.src)
     }
   }, [props.src])
 
   return <img {...props} src={imageSrc} />
+}
+
+// Helpers
+
+function getMimeType(bytes: Uint8Array) {
+  const word = bytes.subarray(0, 4)
+  let header = ""
+  for (let i = 0; i < word.length; i++) {
+    header += word[i].toString(16)
+  }
+
+  switch (header) {
+    case "89504e47":
+      return "image/png"
+    case "47494638":
+      return "image/gif"
+    case "ffd8ffe0":
+    case "ffd8ffe1":
+    case "ffd8ffe2":
+    case "ffd8ffe3":
+    case "ffd8ffe8":
+      return "image/jpeg"
+  }
+
+  const word2 = bytes.subarray(0, 12)
+  header = ""
+  for (let i = 0; i < word2.length; i++) {
+    header += word2[i].toString(16)
+  }
+  if (header.startsWith("52494646") &&
+    header.endsWith("57454250")) {
+    return "image/webp"
+  }
+
+  return "unknown"
 }
