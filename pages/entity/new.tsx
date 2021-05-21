03da8df550e6dbcc9aed8103a2a506812298c81d
lales12 <alexflores120@gmail.com>
2021-05-21 09:28:48 +0200

    Add the entity name as title

--- pages/entity/new.tsx
+++ pages/entity/new.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect } from 'react'
-import { PageCard } from '../../components/cards'
 
-import { EntityCreationPageStep, EntityCreationPageStepTitles } from '../../components/steps-entity-creation'
-import { Column, Grid } from '../../components/grid'
-import { Steps } from '../../components/steps'
-import { MainTitle, MainDescription } from '../../components/text'
-import { useEntityCreation, UseEntityCreationProvider } from '../../hooks/entity-creation'
-import i18n from '../../i18n'
-import { useWallet } from '@hooks/use-wallet'
 import { useDbAccounts } from '@hooks/use-db-accounts'
 import { useHelpCenter } from '@hooks/help-center'
+import { useWallet } from '@hooks/use-wallet'
+
 import { AccountStatus } from '@lib/types'
+
+import {
+  EntityCreationPageStep,
+  EntityCreationPageStepTitles,
+} from '@components/steps-entity-creation'
+import { Column, Grid } from '@components/grid'
+import { Steps } from '@components/steps'
+import { PageCard } from '@components/cards'
+import {
+  useEntityCreation,
+  UseEntityCreationProvider,
+} from '@hooks/entity-creation'
 import { EntityCreationPageSteps } from '@components/steps-entity-creation'
+import { EntityCreationHeader } from '@components/steps-entity-creation/entity-creation-header'
 
 const NewEntity = () => {
-  const { show , hide} = useHelpCenter();
+  const { show, hide } = useHelpCenter()
 
   useEffect(() => {
     show()
@@ -28,11 +35,7 @@ const NewEntity = () => {
     <UseEntityCreationProvider>
       <PageCard>
         <Grid>
-          <Column sm={12} md={5}>
-            <MainTitle>{i18n.t("entity.new_entity")}</MainTitle>
-            <MainDescription>{i18n.t("entity.define_your_credentials_to_protect_the_account")}</MainDescription>
-          </Column>
-
+          <EntityCreationHeader />
           <Column sm={12} md={7}>
             <WizardSteps />
           </Column>
@@ -47,19 +50,20 @@ const NewEntity = () => {
   )
 }
 
+
 const WizardSteps = () => {
   const stepTitles = Object.values(EntityCreationPageStepTitles)
   const { pageStep, methods } = useEntityCreation()
 
-  const { wallet } = useWallet();
-  const { getAccount } = useDbAccounts();
-  
+  const { wallet } = useWallet()
+  const { getAccount } = useDbAccounts()
+
   useEffect(() => {
     if (wallet) {
       const account = getAccount(wallet.address)
 
-      if (!account || account.status === AccountStatus.Ready) return 
-      
+      if (!account || account.status === AccountStatus.Ready) return
+
       methods.setPageStep(EntityCreationPageSteps.CREATION)
       methods.continuePendingProcessCreation(account)
     }
