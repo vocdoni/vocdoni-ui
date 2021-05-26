2960208e4d3b2a168038793db37077d24b047d7c
lales12 <alexflores120@gmail.com>
2021-05-26 20:40:14 +0200

    Landing fixes

--- components/home/components/company-logos/index.tsx
+++ components/home/components/company-logos/index.tsx
@@ -5,9 +5,9 @@ import i18n from '@i18n';
 
 export const CompanyLogos = () => (
   <LogosContainer>
-      <LogoContainer>
+      <SmallContainer>
         <img src='/images/home/company-logo/omnium.png' alt={i18n.t('home.omnium_logo_alt')} />
-      </LogoContainer>
+      </SmallContainer>
  
       <LogoContainer>
         <img src='/images/home/company-logo/pirates.png' alt={i18n.t('home.pirates_logo_alt')} />
@@ -17,18 +17,20 @@ export const CompanyLogos = () => (
         <img src='/images/home/company-logo/cec.png' alt={i18n.t('home.cec_logo_alt')} />
       </LogoContainer>
 
-      <LogoContainer>
+      <SmallContainer>
         <img src='/images/home/company-logo/fairbnb.png' alt={i18n.t('home.fairbnb_logo_alt')} />
-      </LogoContainer>
+      </SmallContainer>
 
-      <LogoContainer>
+      <SmallContainer>
         <img src='/images/home/company-logo/ticanoia.png' alt={i18n.t('home.ticanoia_logo_alt')} />
-      </LogoContainer>
+      </SmallContainer>
   </LogosContainer>
 )
 
 const LogosContainer = styled.div`
   display: flex;
+  padding-top: 20px;
+  align-items: center;
 `
 const LogoContainer = styled.div`
   width: 20%;
@@ -40,7 +42,14 @@ const LogoContainer = styled.div`
 
   & > img {
     max-width: 100%;
-    max-height: 40px;
+    max-height: 36px;
+    margin: auto;
+  }
+`
+const SmallContainer = styled(LogoContainer)`
+  & > img {
+    max-width: 100%;
+    max-height: 30px !important;
     margin: auto;
   }
 `
