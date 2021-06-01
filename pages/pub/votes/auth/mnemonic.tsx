0eca36a46c4f3a0e7f1a59c418d96d3eabe71cb5
lales12 <alexflores120@gmail.com>
2021-06-01 16:54:25 +0200

    Review mnemonic page

--- pages/pub/votes/auth/mnemonic.tsx
+++ pages/pub/votes/auth/mnemonic.tsx
@@ -1,86 +1,122 @@
-import React, { useState } from 'react'
-import { useEntity } from '@vocdoni/react-hooks'
+import React, { useEffect, useState } from 'react'
+import { useEntity, useProcess } from '@vocdoni/react-hooks'
 import { useMessageAlert } from '@hooks/message-alert'
-import { ethers, Wallet } from 'ethers'
-
+import { useUrlHash } from 'use-url-hash'
+import { Wallet, ethers, Wordlist } from 'ethers'
 
 import i18n from '@i18n'
 
 import { ViewContext, ViewStrategy } from '@lib/strategy'
 
-import { useAuthKey } from '@hooks/use-auth-key'
 import { useWallet, WalletRoles } from '@hooks/use-wallet'
 
 import { Loader } from '@components/loader'
 import { VotingErrorPage } from '@components/pub/votes/voting-error-page'
 import { LayoutVoter } from '@components/layout/voter'
-import { Redirect } from '@components/redirect'
-
-import { VOTING_PATH } from '@const/routes'
 import { MnemonicForm } from '@components/pub/votes/auth/link/mnemonic'
-import { langCa as wordListCa } from "@lib/wordlist-ca"
 
+import { InvalidMnemonicError } from '@lib/validators/errors/invalid-mnemonic-error'
+import { langCa as wordListCa } from '@lib/wordlist-ca'
+import { Redirect } from '@components/redirect'
+import { VOTING_PATH } from '@const/routes'
 
 // NOTE: This page uses a custom Layout. See below.
 
+const wordListEs = ethers.wordlists['es']
+const wordListEn = ethers.wordlists['en']
+
 const VoteAuthMnemonic = () => {
-  const {
-    invalidKey,
-    invalidProcessId,
-    loadingInfo,
-    loadingInfoError,
-    processInfo,
-    processId,
-    key,
-  } = useAuthKey()
   const { setAlertMessage } = useMessageAlert()
+  const { setWallet, wallet } = useWallet({ role: WalletRoles.VOTER })
+
   const [mnemonic, setMnemonic] = useState<string>(null)
-  const [invalidWordIndex, setInvalidWordIndex] = useState<number>(0)
-  const [privateKey, setPrivateKey] = useState<string>(null)
-  const { wallet } = useWallet({ role: WalletRoles.VOTER })
+  const [processId, setProcessId] = useState<string>('')
+  const [invalidMnemonic, setInvalidMnemonic] = useState<boolean>(false)
+  const [validating, setValidating] = useState<boolean>(false)
+
+  const urlHash = useUrlHash() // Skip /
+
+  useEffect(() => {
+    if (urlHash) {
+      setProcessId(urlHash.slice(1).split('/')[0])
+    }
+  }, [urlHash])
+
+  const {
+    loading: loadingInfo,
+    error: loadingInfoError,
+    process: processInfo,
+  } = useProcess(processId)
   const { metadata, loading, error } = useEntity(processInfo?.entity)
 
   const ETH_PATH = "m/44'/60'/0'/0/1"
-  const wordList: string[] = []
-  for (let i = 0; i < 2048; i++) wordList.push(wordListCa.getWord(i))
 
-  const HEX_REGEX = /^(0x)?[0-9a-fA-F]+$/
+  const getWalletFromWordList = (newMnemonic: string, wordlist: Wordlist, lang) => {
+    const splittedMnemonic = newMnemonic
+      .trim()
+      .split(' ')
+      .map((w) => w.trim())
+      .filter((w) => !!w)
 
-  const onChange = (newMnemonic: string) => {
-    const inputWords = mnemonic.trim().split(" ").map(w => w.trim()).filter(w => !!w)
-    setMnemonic(mnemonic)
-    for (let i = 0; i < inputWords.length; i++) {
-      if (!wordList.includes(inputWords[i])) {
-        setInvalidWordIndex(i)
-        setPrivateKey(null)
+    for (let i = 0; i < splittedMnemonic.length; i++) {
+      if (wordlist.getWordIndex(splittedMnemonic[i]) === -1) {
+        return undefined
       }
     }
 
+    return Wallet.fromMnemonic(
+      newMnemonic.trim().toLowerCase(),
+      ETH_PATH,
+      wordlist
+    )
+  }
+
+  const getWalletFromMnemonic = (newMnemonic: string) => {
+    const wallet = 
+      getWalletFromWordList(newMnemonic, wordListCa, 'ca') ||
+      getWalletFromWordList(newMnemonic, wordListEn, 'en') ||
+      getWalletFromWordList(newMnemonic, wordListEs, 'es') 
+
+    if (!wallet) {
+      throw new InvalidMnemonicError()
+    }
+
+    return wallet
+  }
+
+  const handleOnChange = (newMnemonic: string) => {
+    setMnemonic(newMnemonic)
+  }
+
+  const handleOnSubmit = () => {
+    setInvalidMnemonic(false)
+    setValidating(true)
+
     try {
-      const wallet = Wallet.fromMnemonic(newMnemonic.trim().toLowerCase(), ETH_PATH, wordListCa)
-      setInvalidWordIndex(-1)
-      setPrivateKey(wallet.privateKey)
-    }
-    catch (err) {
-      setInvalidWordIndex(-1)
-      setPrivateKey(null)
-    }
-
-  }
-
-  const onSubmit = () => {
-    if (!privateKey) {
+      const wallet = getWalletFromMnemonic(mnemonic)
+      console.log('el wallet es', wallet)
+      setWallet(wallet)
+    } catch (error) {
       setAlertMessage(i18n.t('errors.invalid_mnemonic'))
+      setInvalidMnemonic(true)
+      setValidating(false)
     }
-
-
-
-
   }
+
+  const handleOnBlur = () => {
+    setInvalidMnemonic(false)
+
+    try {
+      getWalletFromMnemonic(mnemonic)
+    } catch (error) {
+      setInvalidMnemonic(true)
+    }
+  }
+
   const renderLoadingPage = new ViewStrategy(() => true, <Loader visible />)
 
   const renderVotingInvalidLink = new ViewStrategy(
-    () => !loading && !loadingInfo && invalidProcessId && invalidKey,
+    () => !loading && !loadingInfo && !!loadingInfoError,
     (
       <VotingErrorPage
         message={i18n.t(
@@ -88,6 +124,11 @@ const VoteAuthMnemonic = () => {
         )}
       />
     )
+  )
+
+  const renderInvalidProcessId = new ViewStrategy(
+    () => !processId,
+    <VotingErrorPage message={i18n.t('vote.invalid_process_id_link')} />
   )
 
   const renderLoadingErrorPage = new ViewStrategy(
@@ -95,54 +136,32 @@ const VoteAuthMnemonic = () => {
     <VotingErrorPage message={loadingInfoError} />
   )
 
-  const renderVoteNotSupported = new ViewStrategy(
-    () => !loading && !loadingInfo && !key,
-    (
-      <VotingErrorPage
-        message={i18n.t(
-          'vote.this_type_of_vote_is_not_supported_on_the_current_page'
-        )}
-      />
-    )
-  )
-
   const renderForm = new ViewStrategy(
-    () => !!wallet && !!privateKey,
+    () => !!processInfo,
     (
       <>
-        <Loader visible />
-        <div>
-          <Loader visible={loadingInfo} />
-          <MnemonicForm
-            mnemonic={mnemonic}
-            processInfo={processInfo}
-            entity={metadata}
-            onChange={onChange}
-            onSubmit={onSubmit}
-            submitEnabled={!privateKey}
-          />
-        </div>
+        { !!wallet && <Redirect to={VOTING_PATH + '#/' + processId} /> }
+        <MnemonicForm
+          mnemonic={mnemonic}
+          processInfo={processInfo}
+          entity={metadata}
+          invalidMnemonic={invalidMnemonic}
+          onChange={handleOnChange}
+          onBlur={handleOnBlur}
+          onSubmit={handleOnSubmit}
+          submitEnabled={!validating}
+        />
       </>
     )
   )
 
-  const renderVote = new ViewStrategy(
-    () => !!wallet && privateKey.length > 0,
-    (
-      <>
-        <Loader visible />
-        <Redirect to={VOTING_PATH + '#/' + processId} />
-      </>
-    )
-  )
 
   const viewContext = new ViewContext([
+    renderInvalidProcessId,
+    renderForm,
     renderLoadingErrorPage,
     renderVotingInvalidLink,
-    renderVoteNotSupported,
-    renderForm,
     renderLoadingPage,
-    renderVote,
   ])
 
   return viewContext.getView()
@@ -152,4 +171,3 @@ const VoteAuthMnemonic = () => {
 VoteAuthMnemonic['Layout'] = LayoutVoter
 
 export default VoteAuthMnemonic
-
