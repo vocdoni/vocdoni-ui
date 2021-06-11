d4f03903bebb3286287f3e05767a31159e435f84
lales12 <alexflores120@gmail.com>
2021-06-11 17:03:39 +0200

    Fix empty data on dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -19,9 +19,9 @@ import { LayoutEntity } from '@components/layout/entity'
 // NOTE: This page uses a custom Layout. See below.
 
 const DashboardPage = () => {
-  const [activeVotes, setActiveVotes] = useState<IProcessDetails[]>([])
-  const [upcomingVotes, setUpcomingVotes] = useState<IProcessDetails[]>([])
-  const [votesResults, setVotesResults] = useState<IProcessDetails[]>([])
+  // const [activeVotes, setActiveVotes] = useState<IProcessDetails[]>([])
+  // const [upcomingVotes, setUpcomingVotes] = useState<IProcessDetails[]>([])
+  // const [votesResults, setVotesResults] = useState<IProcessDetails[]>([])
 
   const { wallet } = useWallet()
   const { dbAccounts } = useDbAccounts()
@@ -47,44 +47,75 @@ const DashboardPage = () => {
     : null
   let initialActiveItem = useRef<ProcessTypes>(ProcessTypes.ActiveVotes);
 
-  useEffect(() => {
-    if (loadingProcessList) return
+  // useEffect(() => {
+  //   console.log('rerenderrs')
+  //   const active = []
+  //   const results = []
+  //   const upcoming = []
 
-    const active = []
-    const results = []
-    const upcoming = []
-    console.log(processes)
-    for (let proc of processes.values()) {
-      // info not loaded yet
-      if (!proc || !proc.summary) continue
-      else if (proc.summary?.status === VochainProcessStatus.CANCELED) continue
-      // ignore
-      else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
-      else if (proc.summary?.endBlock < blockHeight) results.push(proc)
-      else if (
-        proc.summary?.status === VochainProcessStatus.ENDED ||
-        proc.summary?.status === VochainProcessStatus.RESULTS
-      ) {
-        results.push(proc)
-      }
-      // Ready or paused
-      else {
-        active.push(proc)
-      }
+  //   for (let proc of processes) {
+  //     // info not loaded yet
+  //     if (!proc || !proc.summary) continue
+  //     else if (proc.summary?.status === VochainProcessStatus.CANCELED) continue
+  //     // ignore
+  //     else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
+  //     else if (proc.summary?.endBlock < blockHeight) results.push(proc)
+  //     else if (
+  //       proc.summary?.status === VochainProcessStatus.ENDED ||
+  //       proc.summary?.status === VochainProcessStatus.RESULTS
+  //     ) {
+  //       results.push(proc)
+  //     }
+  //     // Ready or paused
+  //     else {
+  //       active.push(proc)
+  //     }
+  //   }
+
+  //   initialActiveItem.current = active.length
+  //     ? ProcessTypes.ActiveVotes
+  //     : results.length
+  //       ? ProcessTypes.VoteResults
+  //       : upcoming.length
+  //         ? ProcessTypes.UpcomingVotes
+  //         : ProcessTypes.ActiveVotes
+
+  //   setActiveVotes(active)
+  //   setVotesResults(results)
+  //   setUpcomingVotes(upcoming)
+  // }, [processes])
+
+  const active = []
+  const results = []
+  const upcoming = []
+
+  for (let proc of processes.values()) {
+    // info not loaded yet
+    if (!proc || !proc.summary) continue
+    else if (proc.summary?.status === VochainProcessStatus.CANCELED) continue
+    // ignore
+    else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
+    else if (proc.summary?.endBlock < blockHeight) results.push(proc)
+    else if (
+      proc.summary?.status === VochainProcessStatus.ENDED ||
+      proc.summary?.status === VochainProcessStatus.RESULTS
+    ) {
+      results.push(proc)
     }
+    // Ready or paused
+    else {
+      active.push(proc)
+    }
+  }
 
-    initialActiveItem.current = active.length
-      ? ProcessTypes.ActiveVotes
-      : results.length
-        ? ProcessTypes.VoteResults
-        : upcoming.length
-          ? ProcessTypes.UpcomingVotes
-          : ProcessTypes.ActiveVotes
+  // initialActiveItem.current = active.length
+  //   ? ProcessTypes.ActiveVotes
+  //   : results.length
+  //     ? ProcessTypes.VoteResults
+  //     : upcoming.length
+  //       ? ProcessTypes.UpcomingVotes
+  //       : ProcessTypes.ActiveVotes
 
-    setActiveVotes(active)
-    setVotesResults(results)
-    setUpcomingVotes(upcoming)
-  }, [processIds, loadingProcessList, loadingProcessesDetails])
 
   return (
     <>
@@ -92,9 +123,9 @@ const DashboardPage = () => {
 
       <DashboardActivitySummary
         loading={loadingProcessList || loadingProcessesDetails}
-        activeVotes={activeVotes.length}
-        upcomingVotes={upcomingVotes.length}
-        votesResults={votesResults.length}
+        activeVotes={active.length}
+        upcomingVotes={upcoming.length}
+        votesResults={results.length}
       />
 
       <DashboardProcessList
@@ -102,9 +133,9 @@ const DashboardPage = () => {
         loading={loadingProcessList || loadingProcessesDetails}
         entityMetadata={entityMetadata}
         account={account}
-        activeVotes={activeVotes}
-        votesResults={votesResults}
-        upcomingVoting={upcomingVotes}
+        activeVotes={active}
+        votesResults={results}
+        upcomingVoting={upcoming}
       />
     </>
   )
