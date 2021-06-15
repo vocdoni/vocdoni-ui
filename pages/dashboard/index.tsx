d656d692f7fea0a1d3d893ef9c8faa5ed8e0c46b
emmdim <emmdim@users.noreply.github.com>
2021-06-15 05:37:59 +0200

    Dashboard changes and fixing library versions

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -15,31 +15,27 @@ import { useWallet } from '../../hooks/use-wallet'
 import { useProcessesFromAccount } from '../../hooks/use-processes'
 
 import { LayoutEntity } from '@components/layout/entity'
-import { getVoteStatus, VoteStatus } from '@lib/util'
 
 // NOTE: This page uses a custom Layout. See below.
 
 const DashboardPage = () => {
-  // const [activeVotes, setActiveVotes] = useState<IProcessDetails[]>([])
-  // const [upcomingVotes, setUpcomingVotes] = useState<IProcessDetails[]>([])
-  // const [votesResults, setVotesResults] = useState<IProcessDetails[]>([])
+  const [activeVotes, setActiveVotes] = useState<IProcessDetails[]>([])
+  const [upcomingVotes, setUpcomingVotes] = useState<IProcessDetails[]>([])
+  const [votesResults, setVotesResults] = useState<IProcessDetails[]>([])
 
   const { wallet } = useWallet()
   const { dbAccounts } = useDbAccounts()
   const { blockHeight } = useBlockHeight()
   const { metadata: entityMetadata } = useEntity(wallet?.address)
 
-  // TODO: use loadingProcessList and loadingProcessesDetails to wait until data is loaded
-  // TODO: call useProcessesFromAccount(wallet.address) instead of below
   const {
-    processIds,
     processes,
     loadingProcessList,
     loadingProcessesDetails,
   } = useProcessesFromAccount(wallet?.address)
   // NOTE: processes is a singleton map (for efficiency reasons). This means that no re-render will occur based on `processes`.
   //       Use processIds and loadingProcessList + loadingProcessesDetails instead.
-  console.log(processes)
+
   const hasDbAccountAndWallet = wallet?.address && dbAccounts.length
   const account: Account | null = hasDbAccountAndWallet
     ? dbAccounts.find(
@@ -48,91 +44,44 @@ const DashboardPage = () => {
     : null
   let initialActiveItem = useRef<ProcessTypes>(ProcessTypes.ActiveVotes);
 
-  // useEffect(() => {
-  //   console.log('rerenderrs')
-  //   const active = []
-  //   const results = []
-  //   const upcoming = []
+  useEffect(() => {
+    if (loadingProcessList) return
 
-  //   for (let proc of processes) {
-  //     // info not loaded yet
-  //     if (!proc || !proc.summary) continue
-  //     else if (proc.summary?.status === VochainProcessStatus.CANCELED) continue
-  //     // ignore
-  //     else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
-  //     else if (proc.summary?.endBlock < blockHeight) results.push(proc)
-  //     else if (
-  //       proc.summary?.status === VochainProcessStatus.ENDED ||
-  //       proc.summary?.status === VochainProcessStatus.RESULTS
-  //     ) {
-  //       results.push(proc)
-  //     }
-  //     // Ready or paused
-  //     else {
-  //       active.push(proc)
-  //     }
-  //   }
+    const active = []
+    const results = []
+    const upcoming = []
 
-  //   initialActiveItem.current = active.length
-  //     ? ProcessTypes.ActiveVotes
-  //     : results.length
-  //       ? ProcessTypes.VoteResults
-  //       : upcoming.length
-  //         ? ProcessTypes.UpcomingVotes
-  //         : ProcessTypes.ActiveVotes
-
-  //   setActiveVotes(active)
-  //   setVotesResults(results)
-  //   setUpcomingVotes(upcoming)
-  // }, [processes])
-
-  const active = []
-  const results = []
-  const upcoming = []
-
-  for (let proc of processes) {
-    // info not loaded yet
-    const status: VoteStatus = getVoteStatus(proc?.summary, blockHeight)
-
-    if (!proc || !proc.summary) continue
-    else if (status === VoteStatus.Canceled) continue
-    // ignore
-    else if (status === VoteStatus.Upcoming) upcoming.push(proc)
-    else if (status === VoteStatus.Ended) results.push(proc)
-    // Ready or paused
-    else {
-      active.push(proc)
+    for (let proc of processes.values()) {
+      // info not loaded yet
+      if (!proc || !proc.summary) continue
+      else if (proc.summary?.status === VochainProcessStatus.CANCELED) continue
+      // ignore
+      else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
+      else if (proc.summary?.endBlock < blockHeight) results.push(proc)
+      else if (
+        proc.summary?.status === VochainProcessStatus.ENDED ||
+        proc.summary?.status === VochainProcessStatus.RESULTS
+      ) {
+        results.push(proc)
+      }
+      // Ready or paused
+      else {
+        active.push(proc)
+      }
     }
-  }
 
-  // for (let proc of processes.values()) {
-  //   // info not loaded yet
-  //   const status: VoteStatus = getVoteStatus(proc, blockHeight)
-  //   if (!proc || !proc.summary) continue
-  //   else if (status === VoteStatus.Canceled) continue
-  //   // ignore
-  //   else if (proc.summary?.startBlock > blockHeight) upcoming.push(proc)
-  //   else if (proc.summary?.endBlock < blockHeight) results.push(proc)
-  //   else if (
-  //     proc.summary?.status === VochainProcessStatus.ENDED ||
-  //     proc.summary?.status === VochainProcessStatus.RESULTS
-  //   ) {
-  //     results.push(proc)
-  //   }
-  //   // Ready or paused
-  //   else {
-  //     active.push(proc)
-  //   }
-  // }
+    initialActiveItem.current = active.length
+      ? ProcessTypes.ActiveVotes
+      : results.length
+        ? ProcessTypes.VoteResults
+        : upcoming.length
+          ? ProcessTypes.UpcomingVotes
+          : ProcessTypes.ActiveVotes
 
-  // initialActiveItem.current = active.length
-  //   ? ProcessTypes.ActiveVotes
-  //   : results.length
-  //     ? ProcessTypes.VoteResults
-  //     : upcoming.length
-  //       ? ProcessTypes.UpcomingVotes
-  //       : ProcessTypes.ActiveVotes
-
+    setActiveVotes(active)
+    setVotesResults(results)
+    setUpcomingVotes(upcoming)
+  }, [processes])
 
   return (
     <>
@@ -140,9 +89,9 @@ const DashboardPage = () => {
 
       <DashboardActivitySummary
         loading={loadingProcessList || loadingProcessesDetails}
-        activeVotes={active.length}
-        upcomingVotes={upcoming.length}
-        votesResults={results.length}
+        activeVotes={activeVotes.length}
+        upcomingVotes={upcomingVotes.length}
+        votesResults={votesResults.length}
       />
 
       <DashboardProcessList
@@ -150,9 +99,9 @@ const DashboardPage = () => {
         loading={loadingProcessList || loadingProcessesDetails}
         entityMetadata={entityMetadata}
         account={account}
-        activeVotes={active}
-        votesResults={results}
-        upcomingVoting={upcoming}
+        activeVotes={activeVotes}
+        votesResults={votesResults}
+        upcomingVoting={upcomingVotes}
       />
     </>
   )
