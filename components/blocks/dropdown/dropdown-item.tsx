7095d7b93d39ab72f75736a68360560d6070a2b9
lales12 <alexflores120@gmail.com>
2021-07-12 14:21:16 +0200

    [VPLZ-197] Add edit page and public page

--- components/blocks/dropdown/dropdown-item.tsx
+++ components/blocks/dropdown/dropdown-item.tsx
@@ -40,6 +40,7 @@ export const DropdownItem = ({ children, href, target, disableHover, onClick }: 
 
 export const DropdownItemDivContainer = styled.div<{ hasHover?: boolean }>`
   padding: 16px 15px;
+  width: 100%;
   
   cursor: ${({ hasHover, theme }) => hasHover ? 'pointer' : 'default'};
   ${({ hasHover, theme }) => hasHover ? `&:hover { background-color: ${theme.lightBg}};` : ''}
