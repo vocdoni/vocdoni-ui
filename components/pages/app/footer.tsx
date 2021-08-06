7ad2635e46e79981126a57f88e0bfa4986fb0e3a
lales12 <alexflores120@gmail.com>
2021-08-06 14:02:37 +0200

    [DVOT-12] Add social icons

--- components/pages/app/footer.tsx
+++ components/pages/app/footer.tsx
@@ -10,31 +10,49 @@ export const Footer = () => {
   const theme = useTheme()
   const links = wallet ? LINKS.filter(link => link.logged) : LINKS.filter(link => link.guest)
 
-  return <Container>
-    <LogoSection>
-      <Link href={wallet ? DASHBOARD_PATH : "/"} passHref>
-        <HomeLink target='_self'><img src="/media/logo-full.svg" alt="Vocdoni" /></HomeLink>
-      </Link>
+  return (
+    <Container>
+      <LogoSection>
+        <Link href={wallet ? DASHBOARD_PATH : "/"} passHref>
+          <HomeLink target='_self'><img src="/media/logo-full.svg" alt="Vocdoni" /></HomeLink>
+        </Link>
+      </LogoSection>
 
-    </LogoSection>
+      <SocialLinksSection>
+        <SocialLinkContainer href="https://www.youtube.com/channel/UCt4ZGAb5S8KKZlkdH0P7OzA" target="_blank">
+          <img src="/images/home/footer/youtube.svg" alt="Youtube" />
+        </SocialLinkContainer>
 
+        <SocialLinkContainer href="https://discord.gg/ATF2wqrx" target="_blank">
+          <img src="/images/home/footer/discord.svg" alt="Discord" />
+        </SocialLinkContainer>
 
-    <LinksSection color={theme.lightText}>
-      {links.map(({ url, name, external }, i) => (
-        <NavItem key={name}>
-          <Link href={url} passHref>
-            <ClickableText target={external ? '_blank' : '_self'}>
-              {name}
-            </ClickableText>
-          </Link>
-        </NavItem>
-      ))}
-    <Link href='https://aragon.org/' passHref>
-      <AragonLink target='_blank'><img src="/images/common/powered.svg" alt="Aragon" /></AragonLink>
-    </Link>
-    </LinksSection>
+        <SocialLinkContainer href="https://twitter.com/vocdoni" target="_blank">
+          <img src="/images/home/footer/twitter.svg" alt="Twitter" />
+        </SocialLinkContainer>
 
-  </Container>
+        <SocialLinkContainer href="https://t.me/vocdoni_community" target="_blank">
+          <img src="/images/home/footer/telegram.svg" alt="telegram" />
+        </SocialLinkContainer>
+      </SocialLinksSection>
+
+      <LinksSection color={theme.lightText}>
+        {links.map(({ url, name, external }, i) => (
+          <NavItem key={name}>
+            <Link href={url} passHref>
+              <ClickableText target={external ? '_blank' : '_self'}>
+                {name}
+              </ClickableText>
+            </Link>
+          </NavItem>
+        ))}
+
+        <Link href='https://aragon.org/' passHref>
+          <AragonLink target='_blank'><img src="/images/common/powered.svg" alt="Aragon" /></AragonLink>
+        </Link>
+      </LinksSection>
+    </Container>
+  )
 }
 
 const Container = styled.div`
@@ -50,8 +68,9 @@ const Container = styled.div`
   justify-content: space-between;
   align-items: center;
 
-  @media ${({theme}) => theme.screenMax.mobileL} {
+  @media ${({ theme }) => theme.screenMax.tabletL} {
     height: auto;
+    flex-direction: column;
     position: relative;
     margin-top: -100px;
   }
@@ -60,8 +79,21 @@ const Container = styled.div`
 const NavItem = styled.div`
   margin-right: 30px;
 
-  @media ${({theme}) => theme.screenMax.mobileL} {
-    text-align: center; 
+  @media ${({ theme }) => theme.screenMax.tabletL} {
+    text - align: center; 
+  }
+`
+
+const SocialLinksSection = styled.div`
+  display: flex;
+  align-items: center;
+`
+const SocialLinkContainer = styled.a`
+  margin: 0 6px;
+  
+  & > img {
+    max-width: 20px;
+    max-height: 20px;
   }
 `
 
@@ -74,7 +106,7 @@ const Section = styled.div`
 const LogoSection = styled.div`
   margin-left: 40px;
 
-  @media ${({theme}) => theme.screenMax.mobileL} {
+  @media ${({ theme }) => theme.screenMax.tabletL} {
     margin: 20px auto;
   }
 `
@@ -83,8 +115,15 @@ const LinksSection = styled.div`
   display: flex;
   flex-wrap: wrap;
 
-  @media ${({theme}) => theme.screenMax.mobileL} {
-    flex-direction: column; 
+  @media ${({ theme }) => theme.screenMax.tabletL} {
+    margin-top: 10px;
+    align-items: center;
+    justify-content: center;
+  }
+
+  @media ${({ theme }) => theme.screenMax.mobileL} {
+    flex-direction: column;
+    align-items: center;
     width: 100%;
   }
 `
@@ -109,11 +148,11 @@ const AragonLink = styled.a`
   margin-right: 70px;
 
   & > img {
-    margin-right: 20px;
+    margin - right: 20px;
     height: 32px;
   }
 
-  @media ${({theme}) => theme.screenMax.mobileL} {
+  @media ${({ theme }) => theme.screenMax.tabletL} {
     margin: 20px auto;
   }
 `
