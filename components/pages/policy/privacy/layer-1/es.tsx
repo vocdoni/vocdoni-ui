160a256c3b5a8f926c79c91f30984133fc17aec6
jpaulet <blakdrak@gmail.com>
2021-10-19 19:19:51 +0200

    Changed the terms & conditions texts

--- components/pages/policy/privacy/layer-1/es.tsx
+++ components/pages/policy/privacy/layer-1/es.tsx
@@ -1,63 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
-import {DATA_POLICY_PATH} from '@const/routes'
+import { DATA_POLICY_PATH } from '@const/routes'
 
 export const PrivacyLayer1Es = () => (
   <>
-    <p>
-      En cumplimiento de lo dispuesto por el Reglamento 2016/679, del Parlamento
-      Europeo y del Consejo, de 27 de abril de 2016 (RGPD), Aragon Labs, como
-      responsable del tratamiento, le informa que sus datos personales son
-      precisos para la prestación del servicio contratado, y que estos datos
-      serán objeto de tratamiento, conforme a lo indicado en el registro de las
-      actividades de tratamiento previsto en el artículo 30 del RGPD.
-  </p>
-    <p>Igualmente se le suministra la siguiente información <strong>(Capa 1)</strong>:</p>
+    <p>En cumplimiento de lo dispuesto en el Reglamento (Eu) 2016/679 Del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas físicas en lo que respecta al tratamiento de datos personales y a la libre circulación de estos datos, y por el que se deroga la Directiva 95/46/CE (RGPD), ARAGON LABS, como responsable del tratamiento, le informa que sus datos personales son necesarios para la prestación del servicio contratado, y que los mismos serán tratados, de acuerdo con lo indicado en el registro de actividades de tratamiento previsto en el artículo 30 del RGPD.</p>
+    <p>Igualmente se le suministra la siguiente información<strong>(Capa 1)</strong>:</p>
     <TableWrapper>
       <table>
         <tbody>
           <tr>
-            <td colSpan={2}>Información básica sobre Protección de Datos</td>
-          </tr>
-          <tr>
             <td>Responsable</td>
-            <td>Los datos de carácter personal que se recaban directamente del
-            solicitante del proceso de votación serán tratados de forma confidencial
-            y quedarán incorporados a la correspondiente actividad de tratamiento
-      titularidad de Aragon Labs.</td>
+            <td>Los datos personales que se recojan directamente del solicitante para el proceso de votación, serán tratados de forma confidencial y se incorporarán a la actividad de tratamiento correspondiente propiedad de ARAGON LABS.</td>
           </tr>
           <tr>
             <td>Finalidad</td>
-            <td>  La debida prestación de los servicios de votación electrónica
-      contratados.</td>
+            <td>La correcta prestación de los servicios de voto electrónico contratados, y la gestión comercial y de servicios.</td>
           </tr>
           <tr>
             <td>Legitimación</td>
-            <td> <p>  La debida relación con los solicitantes del servicio de votación electrónica, y la gestión comercial y de servicios.</p>
-              <p>La base legal para el tratamiento de sus datos es la ejecución del contrato a que se refiere el artículo 6.1.b) RGPD.</p>
-              <p>La base legal para el mantenimiento de relaciones comerciales es el interés legítimo de Aragon Labs a que se refiere el artículo 6.1.f) RGPD. </p>
-          </td>
-        </tr>
-        <tr>
-          <td>Destinatarios</td>
-          <td>Sus datos personales no serán cedidos a terceros, ni se transferirán
-            fuera de la Unión Europea</td>
-        </tr>
-        <tr>
-          <td>Derechos</td>
-          <td>Acceder, rectificar y suprimir los datos, así como otros derechos, como
-            se explica en la información adicional</td>
-        </tr>
-        <tr>
-          <td>Información adicional (capa 2)</td>
-          <td>Puede consultar la información adicional y detallada sobre Protección de Datos en nuestra página web:
-            <a target="_blank" href={DATA_POLICY_PATH}>https://vocdoni.app{DATA_POLICY_PATH}</a>
-          </td>
-        </tr>
-        <tr>
-          <td>Fuente (procedencia) de los datos en el supuesto en que los datos personales no hayan sido obtenidos directamente de usted</td>
-          <td>No aplica.</td>
-        </tr>
+            <td> 
+              <p>La base legal para el tratamiento de sus datos es la ejecución del contrato al que se refiere el artículo 6.1.b) GDPR. La base legal para el mantenimiento de las relaciones comerciales, es el interés legítimo de ARAGON LABS referido en el artículo 6.1.f) GDPR</p>
+            </td>
+          </tr>
+          <tr>
+            <td>Destinatarios</td>
+            <td>Sus datos personales no serán cedidos a terceros, ni serán transferidos fuera de la Unión Europea</td>
+          </tr>
+          <tr>
+            <td>Derechos</td>
+            <td>Acceder, rectificar y suprimir los datos, así como otros derechos, como
+              se explica en la información adicional</td>
+          </tr>
+          <tr>
+            <td>Información adicional (capa 2)</td>
+            <td>Puede consultar la información adicional y detallada sobre la protección de datos en nuestro sitio web:
+              <a target="_blank" href={DATA_POLICY_PATH}>https://vocdoni.app{DATA_POLICY_PATH}</a>
+            </td>
+          </tr>
+          <tr>
+            <td>Fuente (procedencia) de los datos en el supuesto en que los datos personales no hayan sido obtenidos directamente de usted</td>
+            <td>No aplica.</td>
+          </tr>
       </tbody>
     </table>
   </TableWrapper>
