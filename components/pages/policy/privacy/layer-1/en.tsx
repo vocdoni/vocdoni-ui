96c12fbf881ab311a23a59848acfb76425dc2fa0
jp_aulet <jpaulet@users.noreply.github.com>
2021-10-25 18:12:25 +0200

    Dvot 148 terms and conditions update (#134)
    * Added Terms & Conditions CA translation
    
    * Added Terms & Conditions EN translation
    
    * Added Terms & Conditions ES translation
    
    * Added the on-page terms & conditions text
    
    * Added the different on-page terms and conditions layout files for the different langs
    
    * Deleted Rudderstack event
    
    * Changed T&C title
    
    * Changed the terms & conditions texts
    
    * Deleted duplicated translation

--- components/pages/policy/privacy/layer-1/en.tsx
+++ components/pages/policy/privacy/layer-1/en.tsx
@@ -2,14 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import { DATA_POLICY_PATH } from '@const/routes'
 
-
 export const PrivacyLayer1En = () => (
   <>
-
-    <p>
-    In compliance with the provisions of Regulation 2016/679, of the European Parliament and of the Council, of April 27, 2016 (GDPR), Aragon Labs, as data controller, informs you that your personal data is accurate for the provision of the contracted service, and that these data will be processed, in accordance with what is indicated in the record of the processing activities provided for in article 30 of the GDPR.
-    </p>
-
+    <p>In compliance with the provisions of Regulation (Eu) 2016/679 Of The European Parliament and of The Council of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (GDPR), ARAGON LABS, as data controller, informs you that your personal data are necessary for the provision of contracted service, and that these data will be processed, in accordance with what is indicated in the record of processing activities provided in article 30 of GDPR.</p>
     <p>The following information is also provided <strong> (Layer 1)</strong>:</p>
     <TableWrapper>
       <table>
@@ -19,32 +14,30 @@ export const PrivacyLayer1En = () => (
         </tr>
           <tr>
             <td>Responsible</td>
-            <td>The personal data that is collected directly from the applicant for the voting process will be treated confidentially and will be incorporated into the corresponding treatment activity owned by Aragon Labs
-</td>
+            <td>The personal data that is collected directly from the applicant for the voting process, will be treated confidentially and will be incorporated into the corresponding treatment activity owned by ARAGON LABS.</td>
           </tr>
           <tr>
             <td>Purpose</td>
-            <td> <p>The proper provision of the contracted electronic voting services.</p>
-<p>The proper relationship with the applicants of the electronic voting service, and the commercial management and services.</p>
-</td>
+            <td> 
+              <p>The proper provision of the contracted electronic voting services, and commercial, and service management.</p>
+            </td>
           </tr>
           <tr>
             <td>Legitimation</td>
-            <td> <p>The legal basis for the processing of your data is the execution of the contract referred to in article 6.1.b) GDPR.</p>
-            <p>The legal basis for maintaining business relationships is the legitimate interest of Aragon Labs referred to in article 6.1.f) GDPR.</p>
+            <td><p>The legal basis for the processing of your data is the execution of the contract referred to in article 6.1.b) GDPR. The legal basis for maintaining business relationships, is the legitimate interest of ARAGON LABS referred to in article 6.1.f) GDPR.</p>
             </td>
           </tr>
           <tr>
             <td>Recipients</td>
-            <td>Your personal data will not be transferred to third parties, nor will it be transferred outside the European Union</td>
+            <td>Your personal data will not be disclosed to third parties, nor will it be transferred outside the European Union</td>
           </tr>
           <tr>
             <td>Rights</td>
-            <td>Access, rectify and delete data, as well as other rights, as explained in the additional information Additional</td>
+            <td>Access, rectify and delete the data, as well as other rights, as explained in the additional information</td>
           </tr>
           <tr>
             <td>Additional Information (layer 2)</td>
-            <td>You can consult the additional information and detailed information on Data Protection on our website::
+            <td>You can consult the additional and detailed information on Data Protection on our website:
             <a target="_blank" href={DATA_POLICY_PATH}>https://vocdoni.app{DATA_POLICY_PATH}</a>
             </td>
           </tr>
