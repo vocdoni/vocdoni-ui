0f0555d70bfee1a558a2c1672390ed51109b2d13
jpaulet <blakdrak@gmail.com>
2021-12-16 18:16:12 +0100

    Added answers in random order in the voting page detail if the param is set. Todo: connect with the metadata value

--- components/pages/pub/votes/components/question-card.tsx
+++ components/pages/pub/votes/components/question-card.tsx
@@ -13,12 +13,41 @@ interface IQuestionProps {
   selectedIndex: number
 }
 
+let shuffle = []
+let isShuffled = []
+
 export const QuestionCard = ({
   question,
   questionIndex,
   onSelectChoice,
   selectedIndex,
 }: IQuestionProps) => {
+
+  function shuffleArray(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+  
+  let randomAnswersOrder = true
+  if (randomAnswersOrder && typeof isShuffled[questionIndex] === 'undefined' && isShuffled[questionIndex] !== true) {
+    shuffle[questionIndex] = [...question.choices]
+    shuffleArray(shuffle[questionIndex])
+    isShuffled[questionIndex] = true
+  }
+
   return (
     <QuestionCardContainer border>
       <Typography variant={TypographyVariant.H4} margin="0">
@@ -31,17 +60,30 @@ export const QuestionCard = ({
       )}
 
       <OptionsContainer>
-        {question.choices.map((option: Choice, index) => (
-          <Radio
-            name={`question-${questionIndex}`}
-            key={index}
-            // value={option.value.toString()}
-            checked={option.value === selectedIndex}
-            onClick={() => onSelectChoice(option.value)}
-          >
-            {option.title.default}
-          </Radio>
+        { randomAnswersOrder && shuffle[questionIndex].map((option: Choice, index) => (
+              <Radio
+                name={`question-${questionIndex}`}
+                key={index}
+                // value={option.value.toString()}
+                checked={option.value === selectedIndex}
+                onClick={() => onSelectChoice(question.choices[question.choices.map((item) => item.value).indexOf(option.value)].value)}
+              >
+                {option.title.default}
+              </Radio>
         ))}
+
+        { !randomAnswersOrder && question.choices.map((option: Choice, index) => (
+            <Radio
+              name={`question-${questionIndex}`}
+              key={index}
+              // value={option.value.toString()}
+              checked={option.value === selectedIndex}
+              onClick={() => onSelectChoice(option.value)}
+            >
+              {option.title.default}
+            </Radio>          
+        ))}
+
       </OptionsContainer>
     </QuestionCardContainer>
   )
