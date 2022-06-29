207135ffbe4a00b036a9de6731e493d67688dfa4
jpa <jaulet@aragonlabs.com>
2022-06-29 13:39:15 +0200

    Customized the vote submitted modal with fcb colors, texts and layout. Added fcb logo.

--- components/pages/pub/votes/components/confirm-modal/vote-submitted.tsx
+++ components/pages/pub/votes/components/confirm-modal/vote-submitted.tsx
@@ -7,6 +7,7 @@ import { SectionText, TextAlign, TextSize } from '@components/elements/text'
 import { FlexContainer, FlexJustifyContent } from '@components/elements/flex'
 import { Button } from '@components/elements/button'
 import { colors } from 'theme/colors'
+import { Spacer, Col, Row, Text } from '@components/elements-v2'
 
 interface IVoteSubmittedProps {
   onAccept: () => void
@@ -16,29 +17,43 @@ export const VoteSubmitted = ({ onAccept }: IVoteSubmittedProps) => {
   const { i18n } = useTranslation()
   return (
     <>
-      <ImageContainer width="320px" justify={FlexJustifyContent.Center}>
+      <ImageContainer width="80px" justify={FlexJustifyContent.Start}>
         <img
-          src="/images/vote/voted-submitted.png"
+          src="/images/app/fcb_logo.png"
           alt={i18n.t('vote.vote_submitted_image_alt')}
         />
       </ImageContainer>
 
       <TextContainer>
-        <SectionText size={TextSize.Big} align={TextAlign.Center}>
+        <SectionText size={TextSize.Big} align={TextAlign.Left} color={colors.FCBBlue}>
           {i18n.t('vote.your_vote_has_been_registered')}
         </SectionText>
-        <SectionText color={colors.lightText}>
-          {i18n.t('vote.thanks_for_participate_on_this_AGM')}
-        </SectionText>
+        <Text size='sm'>
+          {i18n.t('fcb.vote_verification_text')}<a href=''>{i18n.t('fcb.block_explorer')}</a>.
+          {i18n.t('fcb.confirmation_code')}<strong>4d9dac8f566a0ab448efa4c1973579c3d48409aae5d4493ef441bbc7a227dd85</strong>.
+        </Text>
+
+        <Spacer direction='vertical' size='3xl' />
+
+        <Text size='sm'>
+          {i18n.t('fcb.vote_registered')}<strong>22 de juliol a les 00:00 CEST</strong>{i18n.t('fcb.vote_finalized')}
+        </Text>
       </TextContainer>
 
-      <FlexContainer justify={FlexJustifyContent.Center}>
-        <Button positive onClick={onAccept}>{i18n.t('vote.go_back_to_home_page')}</Button>
+      <FlexContainer justify={FlexJustifyContent.Start}>
+        <Button fcb onClick={onAccept}>{i18n.t('vote.go_back_to_home_page')}</Button>
+        <HoritzontalSpacer></HoritzontalSpacer>
+        <Button fcb_border onClick={onAccept}>{i18n.t('app.header.disconnect_account')}</Button>
       </FlexContainer>
     </>
   )
 }
 
 const TextContainer = styled.div`
-  margin-bottom: 12px;
+  margin-bottom: 30px;
+  margin-top: 30px;
+`
+
+const HoritzontalSpacer = styled.div`
+  margin-left: 40px;
 `
