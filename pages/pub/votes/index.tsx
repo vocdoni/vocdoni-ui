5f8b39feb98c59103fcd01d96dce2deaa4d2314b
emmdim <emmdim@users.noreply.github.com>
2022-07-11 18:45:46 +0200

    Adds strategies in voting page, and activates loader
    Signed-off-by: emmdim <emmdim@users.noreply.github.com>

--- pages/pub/votes/index.tsx
+++ pages/pub/votes/index.tsx
@@ -6,18 +6,46 @@ import { VotingErrorPage } from '@components/pages/pub/votes/voting-error-page'
 import { useProcess } from '@vocdoni/react-hooks'
 // import { LayoutVoter } from '@components/pages/app/layout/voter'
 import { LayoutVoter } from '@components/pages/app/layout-v2/voter'
+import { ViewContext, ViewStrategy } from '@lib/strategy'
+import { useProcessWrapper } from '@hooks/use-process-wrapper'
+
 
 // NOTE: This page uses a custom Layout. See below.
 
 const VotingPage = () => {
   const processId = useUrlHash().slice(1) // Skip "/"
-  const { process, error, loading } = useProcess(processId)
+  // const { process, error, loading } = useProcess(processId)
+  const { loadingInfoError, loadingInfo, processInfo: process } = useProcessWrapper(processId)
 
-  if (process) return <VotingPageView />
-  else if (error) return <VotingErrorPage
-    message={error}
-  />
-  return <Loader visible />
+   const renderLoadingPage = new ViewStrategy(
+    () => loadingInfo ,
+    <Loader visible />
+  )
+
+  const renderLoadingErrorPage = new ViewStrategy(
+    () => !!loadingInfoError,
+    <VotingErrorPage message={loadingInfoError} />
+  )
+
+  const renderForm = new ViewStrategy(
+    () => !! process,
+    <VotingPageView />
+  )
+
+  const viewContext = new ViewContext([
+    renderLoadingPage,
+    renderLoadingErrorPage,
+    renderForm,
+  ])
+
+  return viewContext.getView()
+
+
+  // if (process) return <VotingPageView />
+  // else if (loadingInfoError) return <VotingErrorPage
+  //   message={loadingInfoError}
+  // />
+  // return <Loader visible />
 }
 
 // Defining the custom layout to use
