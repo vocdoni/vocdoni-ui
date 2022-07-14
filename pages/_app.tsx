51521b2301e759e2fc7e97757bf78d5261cfab76
emmdim <emmdim@users.noreply.github.com>
2022-07-14 13:59:01 +0200

    Fixes route for web.manifest
    Signed-off-by: emmdim <emmdim@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,7 +67,7 @@ const VocdoniApp: FC<NextAppProps> = ({ Component, pageProps }) => {
             <link rel="apple-touch-icon" sizes="120x120" href="/images/common/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/images/common/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/images/common/favicon-16x16.png" />
-            <link rel="manifest" href="site.webmanifest" />
+            <link rel="manifest" href="/site.webmanifest" />
             <link rel="mask-icon" href="/images/common/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#da532c" />
             <meta name="theme-color" content="#ffffff" />
