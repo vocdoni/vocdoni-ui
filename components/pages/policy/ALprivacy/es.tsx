7db0dd9c6416f5f674bef908ec086c2342ed27d8
jpa <jaulet@aragonlabs.com>
2022-09-19 16:13:23 +0200

    Removed "Aragon Labs", "Aragon Labs AG" and "@aragonlabs.com" references and changed to "Aragon" & "@aragon.org"

--- components/pages/policy/ALprivacy/es.tsx
+++ components/pages/policy/ALprivacy/es.tsx
@@ -2,18 +2,15 @@ import React from 'react'
 
 export const ALPrivacyEs = () => (
   <>
-    <h1>Aragon Labs </h1>
+    <h1>Aragon </h1>
     <h1>Privacy Policy </h1>
 
-    <p >This document sets the privacy rules concerning the use of Aragon Labs AG (the
-    <strong>"Aragon Labs"</strong>) sites and services,
-    including<a
-          href="https://www.google.com/url?q=http://www.aragon.org&amp;sa=D&amp;source=editors&amp;ust=1621981257838000&amp;usg=AOvVaw1NUQvcWG37tngjrQkVXd00">&nbsp;</a>
-</p>
+    <p >This document sets the privacy rules concerning the use of Aragon Association (the
+    <strong>"Aragon"</strong>) sites and services, including</p>
       <p ></p>
       <ul>
         <li >Vocdoni.app - for specification on Vocdoni.app data processing
-      policy and terms and conditions of the service please visit <a  href="https://www.google.com/url?q=https://vocdoni.app&amp;sa=D&amp;source=editors&amp;ust=1621981257839000&amp;usg=AOvVaw3Z0MbkLqeBm4BqEAgUgZdn">https://vocdoni.app</a>
+      policy and terms and conditions of the service please visit <a  href="https://vocdoni.app">https://vocdoni.app</a>
             </li>
 </ul>
         <p ></p>
@@ -24,7 +21,7 @@ export const ALPrivacyEs = () => (
           <li >What information is collected? </li>
         </ol>
         <p ></p>
-        <p >1.1. Aragon Labs may collect the following user&rsquo;s personal data: </p>
+        <p >1.1. Aragon may collect the following user&rsquo;s personal data: </p>
         <p ></p>
         <ul >
           <li >&nbsp;address in the case that
@@ -32,50 +29,50 @@ export const ALPrivacyEs = () => (
         </ul>
         <p ></p>
         <p >1.2 In certain cases the user&rsquo;s consent to use personal information may be
-        required, which can be withdrawn at any time using any of Aragon Lab&rsquo;s contact details. Aragon Labs will get
+        required, which can be withdrawn at any time using any of Aragon contact details. Aragon will get
     in touch with the data subject every time the consent to process any data is required. </p>
         <p ></p>
-        <p >1.3 Aragon Labs may also need to collect and process additional personal information in
+        <p >1.3 Aragon may also need to collect and process additional personal information in
         order to provide services, to perform any contractual obligations, or to comply with the law or any judicial order.
-    In these cases Aragon Labs will specifically get in contact with the data subject.</p>
+    In these cases Aragon will specifically get in contact with the data subject.</p>
         <p ></p>
         <p >2. How is the information collected?</p>
         <p ></p>
-        <p >Aragon Labs processes personal information that the user provides
+        <p >Aragon processes personal information that the user provides
     directly through various channels:</p>
         <p ></p>
         <ul >
-          <li >: when the user registers at any Aragon site, obtains services from Aragon Labs, contacts Aragon Labs with queries or responds to Aragon Labs communications. Additionally, the user may signs up for news, events or newsletters. </li>
+          <li >: when the user registers at any Aragon site, obtains services from Aragon, contacts Aragon with queries or responds to Aragon communications. Additionally, the user may signs up for news, events or newsletters. </li>
         </ul>
         <p >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
         <ul >
           <li >: when
-          the user visits the Site or applies for any of Aragon Labs services, the user may provide Aragon Labs with
+          the user visits the Site or applies for any of Aragon services, the user may provide Aragon with
       personal information that is relevant for the purpose of providing services. </li>
         </ul>
         <p ></p>
         <ul >
           <li >: Aragon
-          Labs also collects data from third parties, acting for that purpose as data processors, including clients,
-          services providers and other entities engaged with Aragon Labs AG for the purposes of providing services, and
-      subject to a specific data processing agreement. </li>
+          also collects data from third parties, acting for that purpose as data processors, including clients,
+          services providers and other entities engaged with Aragon for the purposes of providing services, and
+          subject to a specific data processing agreement. </li>
         </ul>
         <p ></p>
-        <p >Data may also be collected when the user visits any Aragon Labs site, requires any
-          services, or Aragon Labs interacts directly with the user. Aragon Labs may use different technologies to collect and
-          store information such as, the type of device used to access the websites, browser type, the content viewed and features accessed on Aragon Labs' websites,
-          the web pages entered on Aragon Labs websites. For those purposes
-          Aragon Labs may also process data collected by Helpscout (email, browser
+        <p >Data may also be collected when the user visits any Aragon site, requires any
+          services, or Aragon interacts directly with the user. Aragon may use different technologies to collect and
+          store information such as, the type of device used to access the websites, browser type, the content viewed and features accessed on Aragon' websites,
+          the web pages entered on Aragon websites. For those purposes
+          Aragon may also process data collected by Helpscout (email, browser
           fingerprint, IP address), Hubspot, Typeform & Rudderstack.</p>
             <p ></p>
             <p >3. How is the information used?</p>
             <p ></p>
-            <p >Aragon Labs may use the collected information for the following
+            <p >Aragon may use the collected information for the following
     purposes:</p>
             <p ></p>
             <ul >
               <li >Providing services to the user;</li>
-              <li >marketing and business activities in relation to Aragon Labs services,
+              <li >marketing and business activities in relation to Aragon services,
               including newsletters sending, updates, marketing communications and other information that may be of interest to
       the user;</li>
               <li >complying with legal and regulatory obligations;</li>
@@ -83,7 +80,7 @@ export const ALPrivacyEs = () => (
               <li >monitoring the use of websites or other online services for business
               purposes which may include analysis of usage, measurement of site performance and generation of marketing
       reports</li>
-              <li >using it for Aragon Labs legitimate business interests, such as
+              <li >using it for Aragon legitimate business interests, such as
       business research and analysis, managing Aragon &nbsp;sites
       and businesses</li>
                 <li >looking into any complaints or queries</li>
@@ -91,26 +88,26 @@ export const ALPrivacyEs = () => (
                 potential fraud or illegal activities.</li>
                   <li >reviewing and processing any job application in case any user has applied
       for a position</li>
-                  <li >operating Aragon Labs client, customer support, marketing and research
-      services related to the Aragon Labs project</li>
+                  <li >operating Aragon client, customer support, marketing and research
+      services related to the Aragon project</li>
 </ul>
                 <p ></p>
                 <p >4. How the information is shared</p>
                 <p ></p>
-                <p >Aragon Labs shares personal information with other Aragon
-                companies, among the Aragon Association group or third party service providers that assist Aragon Labs providing
+                <p >Aragon shares personal information with other Aragon
+                companies, among the Aragon Association group or third party service providers that assist Aragon providing
     services. This may include sharing information with third party
                   contractors, bound by obligations of confidentiality, in connection with the processing of user&rsquo;s personal
                   information for the purposes described in this Policy, such as, but not limited to, IT and communications service
-                  providers, third parties relevant to the services that Aragon Labs provides including regulators, authorities and
+                  providers, third parties relevant to the services that Aragon provides including regulators, authorities and
     governmental institutions.</p>
                   <p >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-                  <p >Aragon Labs may transfer personal information outside Europe. In those cases Aragon
+                  <p >Aragon may transfer personal information outside Europe. In those cases Aragon
                   will ensure that it is protected and transferred in a manner consistent with legal requirements applicable to the
     information.</p>
                   <p ></p>
                   <p >that
-                  data will be shared between Aragon Labs and the Aragon Associationand other partners of the Aragon Network that
+                  data will be shared between Aragon and the Aragon Associationand other partners of the Aragon Network that
     require the data in order to adequately perform their duties. </p>
                   <p ></p>
                   <p >5. Information Security</p>
@@ -120,42 +117,42 @@ export const ALPrivacyEs = () => (
                   <p ></p>
                   <p >keeps the information for a limited period of
                   time and strictly as long as is necessary for the relevant purpose - and also for as long as it is necessary to
-                  comply with legal obligations, laws or regulations, which may set a minimum period for which Aragon Labs has to keep
+                  comply with legal obligations, laws or regulations, which may set a minimum period for which Aragon has to keep
     the users personal information.</p>
                   <p ></p>
                   <p >6. Exercise of Rights</p>
                   <p ></p>
                   <p >The user holds a number of legal rights in
-                  relation to the personal information that Aragon Labs may hold about the user. These rights can be exercised at any
-    time by contacting Aragon Labs using the details set out below.</p>
+                  relation to the personal information that Aragon may hold about the user. These rights can be exercised at any
+    time by contacting Aragon using the details set out below.</p>
                   <p ></p>
                   <p >The user has the right to:</p>
                   <p ></p>
                   <ul >
                     <li >Obtain information regarding the processing of personal information and
       access to the personal information which is held about the user;</li>
-                    <li >Request that Aragon Labs corrects any personal information if it is
+                    <li >Request that Aragon corrects any personal information if it is
       inaccurate or incomplete;</li>
-                    <li >Request that Aragon Labs erase the personal
-                    information in certain circumstances. There may be circumstances where the user asks Aragon Labs to erase personal
-      information but Aragon Labs is legally entitled to retain it.</li>
-                    <li >Object to, and request that Aragon Labs restricts the processing of
+                    <li >Request that Aragon erase the personal
+                    information in certain circumstances. There may be circumstances where the user asks Aragon to erase personal
+      information but Aragon is legally entitled to retain it.</li>
+                    <li >Object to, and request that Aragon restricts the processing of
                     personal information in certain circumstances. There may be circumstances where the user objects to, or asks
-                    Aragon Labs to restrict the processing of personal information but Aragon Labs is legally entitled to refuse that
+                    Aragon to restrict the processing of personal information but Aragon is legally entitled to refuse that
       request.</li>
                     <li >Receive some personal information in a structured, commonly used and
-      machine-readable format and/or request that Aragon Labs transmits that information to a third party.</li>
+      machine-readable format and/or request that Aragon transmits that information to a third party.</li>
                     <li >Withdraw any consent, although in certain circumstances it may be
-                    lawful for us to continue processing without the consent if Aragon Labs has another legitimate reason (other than
+                    lawful for us to continue processing without the consent if Aragon has another legitimate reason (other than
       consent) for doing so.</li>
                     <li >Lodge a complaint with the relevant data protection authority, if any
-      rights have been infringed by Aragon Labs. </li>
+      rights have been infringed by Aragon. </li>
                   </ul>
                   <p >7. Contact</p>
                   <p ></p>
                   <p >For any further information on the collection, use, disclosure,
                   transfer or processing of personal information or the exercise of any of the rights listed above, please contact
-    Aragon by sending an email to <a>privacy@aragonlabs.org</a></p>
+    Aragon by sending an email to <a>privacy@aragon.org</a></p>
 
   </>
 )
