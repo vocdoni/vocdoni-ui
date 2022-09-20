1a527ae64ba5b8bc10347e898e1777f08f87b8cc
jpa <jaulet@aragonlabs.com>
2022-09-20 17:30:27 +0200

    Added "Application" component around DateTimePicker for allowing customization. Added theme color palette (vocdoni green) for the DateTimePicker

--- components/pages/votes/new/components/date-selector.tsx
+++ components/pages/votes/new/components/date-selector.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { PreregisterTimeline } from "./preregister-timeline"
 import { RadioButtonGroup } from "./radio-buttons"
+import { Application } from 'react-rainbow-components'
 
 export enum StartOptions {
   Inmediatly = 'inmediatly',
@@ -86,6 +87,15 @@ export const DateSelector = (props: IDateSelectorProps) => {
   const handleChangeEndOption = (e) => {
     setEndOption(e)
   }
+
+  const theme = {
+    rainbow: {
+      palette: {
+        brand: '#46C4C2',
+      },
+    },
+  }
+
   return (
     <Card variant='outlined' padding='24px 40px'>
       <Row gutter="xl">
@@ -111,14 +121,16 @@ export const DateSelector = (props: IDateSelectorProps) => {
                 }
                 {/* INPUT */}
                 <Col xs={11} disableFlex>
-                  <DateTimePicker
-                    value={startDate}
-                    placeholder={i18n.t("votes.new.select_date_and_time")}
-                    error={invalidStartDate}
-                    minDate={addOffsetToDate(new Date(), 0, 0, MIN_DELAY_TIME)}
-                    onChange={(value) => setStartDate(value)}
-                    disabled={startOption === StartOptions.Inmediatly}
-                  />
+                  <Application theme={theme}>
+                    <DateTimePicker
+                      value={startDate}
+                      placeholder={i18n.t("votes.new.select_date_and_time")}
+                      error={invalidStartDate}
+                      minDate={addOffsetToDate(new Date(), 0, 0, MIN_DELAY_TIME)}
+                      onChange={(value) => setStartDate(value)}
+                      disabled={startOption === StartOptions.Inmediatly}
+                    />
+                  </Application>
                 </Col>
               </Row>
             </Col>
@@ -143,12 +155,14 @@ export const DateSelector = (props: IDateSelectorProps) => {
                 }
                 {/* INPUT */}
                 <Col xs={11} disableFlex>
-                  <DateTimePicker
-                    value={endDate}
-                    placeholder={i18n.t("votes.new.select_date_and_time")}
-                    error={invalidStartDate}
-                    onChange={(value) => setEndDate(value)}
-                  />
+                  <Application theme={theme}>
+                    <DateTimePicker
+                      value={endDate}
+                      placeholder={i18n.t("votes.new.select_date_and_time")}
+                      error={invalidStartDate}
+                      onChange={(value) => setEndDate(value)}
+                    />
+                  </Application>
                 </Col>
               </Row>
             </Col>
